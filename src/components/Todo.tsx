fd1aed7a19400ab02761aa4995d888fe7972f51a
Bruno Mateus <bruno_205@hotmail.com>
2022-08-09 09:56:08 -0300

    first commit

---
+++ src/components/Todo.tsx
@@ -0,0 +1,32 @@
+import styles from './todo.module.css'
+import { Trash, Check } from 'phosphor-react'
+import { ChangeEvent } from 'react';
+
+
+interface ITodo {
+  id: string;
+  content: string;
+  isComplete: boolean;
+}
+
+interface TodoProps {
+  todo: ITodo;
+  onDeleteTodo: (todo: ITodo) => void;
+  todoComplete(event: ChangeEvent<HTMLInputElement>): void;
+}
+
+export function Todo({ todo, onDeleteTodo, todoComplete }: TodoProps) {
+  return (
+    <div className={styles.todo}>
+
+      <label className={styles.container} >
+        <input type="checkbox" value={todo.id} onChange={(e) => todoComplete(e)} />
+        <span className={styles.checkmark}><Check /></span>
+        <p>{todo.content}</p>
+      </label>
+      <button
+        onClick={() => onDeleteTodo(todo)}
+      ><Trash size={20} color={'var(--gray-300)'} /></button>
+    </div>
+  )
+}
---
+++ src/components/Todos.tsx
@@ -0,0 +1,125 @@
+import styles from './todos.module.css'
+import { PlusCircle } from 'phosphor-react'
+import clipboard from '../assets/Clipboard.svg'
+import { Todo } from './Todo'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
+import { v4 as uuid } from 'uuid'
+
+interface ITodo {
+  id: string
+  content: string;
+  isComplete: boolean;
+}
+
+export function Todos() {
+
+  const [getTodo, setGetTodo] = useState<ITodo>()
+  const [todos, setTodos] = useState<ITodo[]>([])
+  const [todosComplete, setTodosComplete] = useState<ITodo[]>([])
+  const [textAreaValue, setTextAreaValue] = useState('')
+  function handleInvalidTodo(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('Campo obrigatório')
+  }
+
+  function handleNewTodo(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('');
+
+    setTextAreaValue(event.target.value)
+    setGetTodo({
+      id: uuid(),
+      content: event.target.value,
+      isComplete: false
+    })
+
+
+
+  }
+
+  function handleCreateNewTodo(event: FormEvent) {
+    event.preventDefault()
+    //@ts-ignore
+    setTodos([...todos, getTodo])
+
+
+    setTextAreaValue('')
+  }
+
+
+
+  function deleteTodo(todo: ITodo) {
+    const todoWithoutDeleteOne = todos.filter(todos => todos.id !== todo.id)
+    console.log(todoWithoutDeleteOne)
+    setTodos([...todoWithoutDeleteOne])
+
+    const completedTodosDeleted = todoWithoutDeleteOne.filter(todo => todo.isComplete === true)
+
+    setTodosComplete([...completedTodosDeleted])
+
+  }
+
+  function handleSelectTodoCompleted(event: ChangeEvent<HTMLInputElement>) {
+
+
+    const checkedTodos = todos.map(todo => {
+
+      if (todo.id === event.target.value) todo.isComplete = event.target.checked
+      return todo
+    })
+
+    const completedTodos = checkedTodos.filter(todo => todo.isComplete === true)
+
+    setTodosComplete([...completedTodos])
+
+  }
+  const hasTodo = textAreaValue.length === 0
+
+  return (
+    <div className={styles.todos}>
+      <form>
+        <textarea placeholder="Adicione uma nova tarefa"
+          required={true}
+          onChange={handleNewTodo}
+          value={textAreaValue}
+          onInvalid={handleInvalidTodo}
+        />
+        <button
+          type="submit"
+          onClick={handleCreateNewTodo}
+          disabled={hasTodo}
+        >Criar<PlusCircle size={16} /></button>
+      </form>
+      <div className={styles.todosList}>
+        <header>
+          <span>Tarefas criadas <span>{todos.length}</span></span>
+          <span>Tarefas concluídas <span>{todosComplete.length}</span></span>
+        </header>
+        <div>
+          <form className={styles.todoContent}>
+            {
+              todos.map((todo) => {
+                return (
+                  <Todo
+                    key={todo.id}
+                    todo={todo}
+                    onDeleteTodo={deleteTodo}
+                    todoComplete={handleSelectTodoCompleted}
+                  />
+                )
+              })
+            }
+
+            <div className={todos.length === 0 ? styles.noneTodos : styles.noneTodoHidden}>
+              <img src={clipboard} alt="adicione um todo" />
+
+              <p>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+              </p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  )
+}
